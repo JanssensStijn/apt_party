0484ef0aeffe7ab77316f4784265d5dc587c2d4d
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2023-09-18 22:04:55 +0200

    dynamic pages with common css

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,18 +1,27 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping({"/", "/home"})
-    public String home() {
+    public String home(Model model) {
+        int myCalculatedValue = 5 * 12;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
         return "home";
     }
 
     @GetMapping("/about")
-    public String about() {
+    public String about(Model model) {
+        String myName = "Stijn Janssens";
+        String myStreet = "Braambessenweg 2";
+        String myCity = "Aarschot";
+        model.addAttribute("myName", myName);
+        model.addAttribute("myStreet", myStreet);
+        model.addAttribute("myCity", myCity);
         return "about";
     }
 }
