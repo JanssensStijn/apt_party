1f25708852be9a61b8674c74015646f155421bb2
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2023-09-26 21:11:23 +0200

    5.2 using venueRepository

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -1,16 +1,22 @@
 package be.thomasmore.party.controllers;
 
 import be.thomasmore.party.model.Venue;
+import be.thomasmore.party.repositories.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class VenueController {
+    @Autowired
+    private VenueRepository venueRepository;
 
     @GetMapping({"/venuedetails"})
     public String venuedetails(Model model) {
-        Venue myVenue = new Venue();
+        /*Venue myVenue = new Venue();
         myVenue.setVenueName("Boesj");
         myVenue.setLinkMoreInfo("https://sass-lang.com/");
         myVenue.setCapacity(100);
@@ -20,7 +26,10 @@ public class VenueController {
         myVenue.setFreeParkingAvailable(false);
         myVenue.setCity("Mechelen");
         myVenue.setDistanceFromPublicTransportInKm(1);
-        model.addAttribute("myVenue", myVenue);
+        model.addAttribute("Venue", myVenue);*/
+
+        Optional<Venue> venueFromDb = venueRepository.findById(1);
+        if (venueFromDb.isPresent()) model.addAttribute("venue", venueFromDb.get());
         return "venuedetails";
     }
 }
