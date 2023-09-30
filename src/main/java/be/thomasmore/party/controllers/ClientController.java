c7f8fb6bf1f5d3077cf644addab86c27fd249445
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2023-09-30 13:45:24 +0200

    5.4 clientdetails en clienthome

--- src/main/java/be/thomasmore/party/controllers/ClientController.java
+++ src/main/java/be/thomasmore/party/controllers/ClientController.java
@@ -18,23 +18,52 @@ public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
 
-    @GetMapping({"/clientgreeting"})
+    @GetMapping({"/clienthome"})
+    public String clienthome(Model model) {
+        model.addAttribute("appName", appName);
+        Optional<Client> clientFromDb = clientRepository.findById(1);
+        String message = "";
+        if (clientFromDb.isPresent()){
+            message = "klant \"" + clientFromDb.get().getName() + "\" gevonden";
+        }
+        else message = "deze klant kan niet gevonden worden";
+        model.addAttribute("message", message);
+        return "clienthome";
+    }
+
+    @GetMapping({"/clientdetails"})
     public String clientdetails(Model model) {
+        model.addAttribute("appName", appName);
+        Optional<Client> clientFromDb = clientRepository.findById(1);
+        String message = "";
+        if (clientFromDb.isPresent()){
+            double discount = 0;
+            if(clientFromDb.get().getTotalAmount() >= 100) discount = clientFromDb.get().getTotalAmount() * 0.005;
+            message = clientFromDb.get().getName() + ", je discount is " + discount + "EUR";
+        }
+        else message = "deze klant kan niet gevonden worden";
+        model.addAttribute("message", message);
+        return "clientdetails";
+    }
+
+    @GetMapping({"/clientgreeting"})
+    public String clientgreeting(Model model) {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH");
         LocalDateTime timeNow = LocalDateTime.now();
         int currentHour = Integer.parseInt(timeNow.format(formatter));
 
         String greetingPart1;
-        if (currentHour >= 6 && currentHour < 12) greetingPart1 = "Goedemorgen";
-        else if (currentHour >= 12 && currentHour < 17) greetingPart1 = "Goedemiddag";
-        else if (currentHour >= 17 && currentHour < 22) greetingPart1 = "Goedenavond";
-        else  greetingPart1 = "Goedennacht";
+        if (currentHour >= 6 && currentHour < 12) greetingPart1 = "Goedemorgen ";
+        else if (currentHour >= 12 && currentHour < 17) greetingPart1 = "Goedemiddag ";
+        else if (currentHour >= 17 && currentHour < 22) greetingPart1 = "Goedenavond ";
+        else  greetingPart1 = "Goedennacht ";
 
         model.addAttribute("appName", appName);
         model.addAttribute("greetingPart1", greetingPart1);
 
         Optional<Client> clientFromDb = clientRepository.findById(1);
+        String message = "";
         if (clientFromDb.isPresent()){
             String greetingPart2 = "", greetingPart3 = "";
             if(clientFromDb.get().getNrOfOrders() == 0) {
@@ -53,11 +82,14 @@ public class ClientController {
                 greetingPart2 = "allerliefste ";
                 greetingPart3 = ", jij bent een topper!";
             }
-            model.addAttribute("client", clientFromDb.get());
+            //model.addAttribute("client", clientFromDb.get());
 
-            model.addAttribute("greetingPart2", greetingPart2);
-            model.addAttribute("greetingPart3", greetingPart3);
-        }
+            //model.addAttribute("greetingPart2", greetingPart2);
+            //model.addAttribute("greetingPart3", greetingPart3);
+
+            message = greetingPart1 + greetingPart2 + clientFromDb.get().getName() + greetingPart3;
+        }else message = "deze klant kan niet gevonden worden";
+        model.addAttribute("message", message);
         return "clientgreeting";
     }
 }
