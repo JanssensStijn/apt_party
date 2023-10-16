7658795dc06397930a356172727c22210edca61d
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2023-10-16 21:35:02 +0200

    1.2 Exercise Verwijder de filter buttons

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -16,6 +16,8 @@ public class VenueController {
     @Autowired
     private VenueRepository venueRepository;
 
+
+
     @GetMapping({"/venuedetails/{id}", "/venuedetails", "/venuedetails/"})
     public String venuedetails(Model model, @PathVariable(required = false) Integer id) {
 
@@ -36,62 +38,6 @@ public class VenueController {
         }
 
         return "venuedetails";
-    }
-
-    @GetMapping({"/venuelist", "/venuelist/","/venuelist/{filter}","/venuelist/{filter}/","/venuelist/outdoor/all", "/venuelist/indoor/all", "/venuelist/capacity/all" })
-    public String venuelist(Model model, @PathVariable(required = false) String filter) {
-        final Iterable<Venue> allVenues = venueRepository.findAll();
-        model.addAttribute("venues", allVenues);
-        model.addAttribute("outdoorFilter", "all");
-        model.addAttribute("indoorFilter", "all");
-        model.addAttribute("capacityFilter", "all");
-        return "venuelist";
-    }
-
-    @GetMapping({"/venuelist/outdoor/{filter}"})
-    public String venuelistOutdoor(Model model, @PathVariable(required = false) String filter) {
-        final Iterable<Venue> allVenues = venueRepository.findByOutdoor(filter.equals("yes"));
-        model.addAttribute("venues", allVenues);
-        if(filter.equals("yes")||filter.equals("no")) model.addAttribute("outdoorFilter", filter );
-        else model.addAttribute("outdoorFilter", "no" );
-        model.addAttribute("indoorFilter", "all" );
-        model.addAttribute("capacityFilter", "all");
-        return "venuelist";
-    }
-
-    @GetMapping({"/venuelist/indoor/{filter}"})
-    public String venuelistIndoor(Model model, @PathVariable(required = false) String filter) {
-        final Iterable<Venue> allVenues = venueRepository.findByIndoor(filter.equals("yes"));
-        model.addAttribute("venues", allVenues);
-        if(filter.equals("yes")||filter.equals("no")) model.addAttribute("indoorFilter", filter );
-        else model.addAttribute("indoorFilter", "no" );
-        model.addAttribute("outdoorFilter", "all" );
-        model.addAttribute("capacityFilter", "all");
-        return "venuelist";
-    }
-
-    @GetMapping({"/venuelist/capacity/{filter}"})
-    public String venuelistCapacity(Model model, @PathVariable(required = false) String filter) {
-        switch(filter){
-            case "S":  final Iterable<Venue> allSmallVenues = venueRepository.findByCapacityLessThan(200);
-                        model.addAttribute("venues", allSmallVenues);
-            break;
-            case "M":  final Iterable<Venue> allMediumVenues = venueRepository.findByCapacityBetween(200, 600);
-                        model.addAttribute("venues", allMediumVenues);
-                break;
-            case "L":  final Iterable<Venue> allLargeVenues = venueRepository.findByCapacityGreaterThan(600);
-                        model.addAttribute("venues", allLargeVenues);
-                break;
-            default:  final Iterable<Venue> allVenues = venueRepository.findAll();
-                model.addAttribute("venues", allVenues);
-            break;
-        }
-
-        if(filter.equals("S")||filter.equals("M")||filter.equals("L")) model.addAttribute("capacityFilter", filter );
-        else model.addAttribute("capacityFilter", "all");
-        model.addAttribute("indoorFilter", "all");
-        model.addAttribute("outdoorFilter", "all" );
-        return "venuelist";
     }
 
     @GetMapping({"/error"})
