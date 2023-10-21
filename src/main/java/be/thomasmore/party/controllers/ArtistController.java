01eb23f68787762dba7ad1dc83418c51a3e2acf6
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2023-10-21 14:07:27 +0200

    3.7 Exercise: Eenvoudige filter voor artistlist

--- src/main/java/be/thomasmore/party/controllers/ArtistController.java
+++ src/main/java/be/thomasmore/party/controllers/ArtistController.java
@@ -3,16 +3,21 @@ package be.thomasmore.party.controllers;
 import be.thomasmore.party.model.Artist;
 import be.thomasmore.party.model.Venue;
 import be.thomasmore.party.repositories.ArtistRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.w3c.dom.Text;
 
 import java.util.Optional;
 
 @Controller
 public class ArtistController {
+    private Logger logger = LoggerFactory.getLogger(VenueController.class);
 
     @Autowired
     private ArtistRepository artistRepository;
@@ -20,9 +25,37 @@ public class ArtistController {
     @GetMapping({"/artistlist"})
     public String artistlist(Model model) {
         final Iterable<Artist> allArtists = artistRepository.findAll();
+        final long numberOfArtists = artistRepository.count();
+        model.addAttribute("numberOfArtists" , numberOfArtists);
         model.addAttribute("artists", allArtists);
+        model.addAttribute("showFilter", false);
         return "artistlist";
     }
+
+    @GetMapping({"/artistlist/filter"})
+    public String artistListWithFilter(Model model,
+                                      @RequestParam(required = false) String textFilter ){
+
+        logger.info("artistlistWithFilter -- name = "+ textFilter);
+        final Iterable<Artist> allArtists;
+        final long numberOfArtists;
+
+        if(textFilter != null){
+            allArtists = artistRepository.findByArtistNameContainsIgnoreCase(textFilter);
+            numberOfArtists = artistRepository.findAllByArtistNameContainsIgnoreCase(textFilter).size();
+        }
+        else {
+            allArtists = artistRepository.findAll();
+            numberOfArtists = artistRepository.count();
+        }
+
+        model.addAttribute("textFilter" , textFilter);
+        model.addAttribute("numberOfArtists" , numberOfArtists);
+        model.addAttribute("artists", allArtists);
+        model.addAttribute("showFilter", true);
+        return "artistlist";
+    }
+
     @GetMapping({"/artistdetails/{id}","/artistdetails", "/artistdetails/"})
     public String artistdetails(Model model, @PathVariable (required = false) Integer id) {
         if (id == null) return "artistdetails";
--- src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
+++ src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
@@ -3,6 +3,7 @@ package be.thomasmore.party.repositories;
 import be.thomasmore.party.model.Artist;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArtistRepository extends CrudRepository<Artist, Integer> {
@@ -13,4 +14,7 @@ public interface ArtistRepository extends CrudRepository<Artist, Integer> {
     Optional<Artist> findFirstByIdLessThanOrderByIdDesc(Integer id);
 
     Optional<Artist> findFirstByOrderByIdDesc();
+
+    Iterable<Artist> findByArtistNameContainsIgnoreCase(String keyword);
+    List<Artist> findAllByArtistNameContainsIgnoreCase(String keyword);
 }
